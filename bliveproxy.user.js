6854e92dc5feb02e8706bdd22d2a170cb627bfba
John Smith <xfgryujk@126.com>
2021-05-15 00:33:39 +0800

    修复消息头ver

--- bliveproxy.user.js
+++ bliveproxy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bliveproxy
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  B站直播websocket hook框架
 // @author       xfgryujk
 // @include      /https?:\/\/live\.bilibili\.com\/?\??.*/
@@ -23,8 +23,8 @@
 (function() {
   const HEADER_SIZE = 16
 
-  const WS_BODY_PROTOCOL_VERSION_INFLATE = 0
-  const WS_BODY_PROTOCOL_VERSION_NORMAL = 1
+  const WS_BODY_PROTOCOL_VERSION_NORMAL = 0
+  const WS_BODY_PROTOCOL_VERSION_HEARTBEAT = 1
   const WS_BODY_PROTOCOL_VERSION_DEFLATE = 2
   const WS_BODY_PROTOCOL_VERSION_BROTLI = 3
 
@@ -121,8 +121,8 @@
     let packLen = HEADER_SIZE + body.byteLength
     let packet = new ArrayBuffer(packLen)
 
-    // 不需要DEFLATE
-    let ver = operation === OP_HEARTBEAT_REPLY ? WS_BODY_PROTOCOL_VERSION_INFLATE : WS_BODY_PROTOCOL_VERSION_NORMAL
+    // 不需要压缩
+    let ver = operation === OP_HEARTBEAT_REPLY ? WS_BODY_PROTOCOL_VERSION_HEARTBEAT : WS_BODY_PROTOCOL_VERSION_NORMAL
     let packetView = new DataView(packet)
     packetView.setUint32(0, packLen)        // pack_len
     packetView.setUint16(4, HEADER_SIZE)    // raw_header_size
@@ -150,18 +150,24 @@
       let body = new Uint8Array(data.buffer, offset + HEADER_SIZE, packLen - HEADER_SIZE)
       if (operation === OP_SEND_MSG_REPLY) {
         switch (ver) {
-        case WS_BODY_PROTOCOL_VERSION_BROTLI:
-          body = BrotliDecode(body)
-          handleMessage(body, callRealOnMessageByPacket)
+        case WS_BODY_PROTOCOL_VERSION_NORMAL:
+          body = textDecoder.decode(body)
+          body = JSON.parse(body)
+          handleCommand(body, callRealOnMessageByPacket)
           break
         case WS_BODY_PROTOCOL_VERSION_DEFLATE:
           body = pako.inflate(body)
           handleMessage(body, callRealOnMessageByPacket)
           break
-        default:
-          body = JSON.parse(textDecoder.decode(body))
-          handleCommand(body, callRealOnMessageByPacket)
+        case WS_BODY_PROTOCOL_VERSION_BROTLI:
+          body = BrotliDecode(body)
+          handleMessage(body, callRealOnMessageByPacket)
           break
+        default: {
+          let packet = makePacketFromUint8Array(body, operation)
+          callRealOnMessageByPacket(packet)
+          break
+        }
         }
       } else {
         let packet = makePacketFromUint8Array(body, operation)
